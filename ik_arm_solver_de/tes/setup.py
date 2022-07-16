3e3e65b5f4ae97a0a64920deb7cf75ba21438880
Julian Reza Erwandi <73013027+jrerwandi@users.noreply.github.com>
2022-07-17 01:03:43 +0700

    Delete ik_arm_solver_de/tes directory

--- ik_arm_solver_de/tes/setup.py
+++
@@ -1,7 +0,0 @@
-from distutils.core import setup
-from catkin_pkg.python_setup import generate_distutils_setup
-d = generate_distutils_setup(
-    packages=['beginner_tutorials'],
-    package_dir={'': 'src'}
-)
-setup(**d)
