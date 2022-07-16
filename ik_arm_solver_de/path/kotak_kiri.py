5b36837ecb3e0e2b6ad39dc043246b9635b7e1b1
Julian Reza Erwandi <73013027+jrerwandi@users.noreply.github.com>
2022-07-17 01:03:18 +0700

    Update kotak_kiri.py

--- ik_arm_solver_de/path/kotak_kiri.py
+++ ik_arm_solver_de/path/kotak_kiri.py
@@ -9,39 +9,38 @@ rospy.init_node('by_path', anonymous=True)
 path_pub = rospy.Publisher('trajectory', Path, queue_size = 10)
 pose_pub = rospy.Publisher('pose', PoseStamped, queue_size = 10)
 rate = rospy.Rate(1)
-hehe = False
-abc = True
-a = 0.01
-b = 0.01
+start_delay = False #gambar pathnya dulu, setelah path muncul delay untuk publish aktif
+move_y = True
+point_y = 0.01
+point_z = 0.01
 y = 0.2
 z = 0.2
-j = 0
 
 def run():	
-	global hehe,abc,a,b,y,z,j
+	global start_delay,move_y,point_y,point_z,y,z
 	seq = 0
 	path = Path()
 	path.header.stamp = rospy.Time.now()
 	path.header.frame_id="base_link"
-	for i in range(50):
-		if(abc):
-			y += a
-			if (i >= 49):
-				hehe = True
-			if hehe:
+	for point in range(50):
+		if(move_y):
+			y += point_y
+			if (point >= 49):
+				start_delay = True
+			if start_delay:
 				time.sleep(1)
 			if (y <= 0.2 or y >= 0.3):
-				a *= -1
-				abc = False		
+				point_y *= -1
+				move_y = False		
 		else:
-			z += b
-			if (i >= 49):
-				hehe = True
-			if hehe:
+			z += point_z
+			if (point >= 49):
+				start_delay = True
+			if start_delay:
 				time.sleep(1)
 			if (z >= 0.3 or z <= 0.2):
-				b *= -1
-				abc = True
+				point_z *= -1
+				move_y = True
 		if (y >= 0.3):
 			y = 0.3
 		elif (y <= 0.2):
